2082c9c150d22ca4b1fbd9d6a20f77f1c0bf2f7c
wu <connor@git.com>
2022-11-19 17:55:01 +0800

    add card

--- app/src/main/java/com/connor/splashpose/ui/main/HomePhotos.kt
+++ app/src/main/java/com/connor/splashpose/ui/main/HomePhotos.kt
@@ -1,13 +1,18 @@
 package com.connor.splashpose.ui.main
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.paging.LoadState
 import androidx.paging.PagingData
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
@@ -31,12 +36,58 @@ fun PhotoList(photos: Flow<PagingData<PhotoItem>>) {
     val lazyPhotoItems = photos.collectAsLazyPagingItems()
     LazyColumn {
         items(lazyPhotoItems) { photo ->
+            CardPhoto(photo)
+        }
+        lazyPhotoItems.apply {
+            when {
+                loadState.refresh is LoadState.Loading -> {
+                    item { Text(text = "Loading", modifier = Modifier.fillParentMaxSize()) }
+                }
+                loadState.refresh is LoadState.Error -> {
+                    item { Text(text = "Error", modifier = Modifier.fillParentMaxSize()) }
+                }
+                loadState.append is LoadState.Loading -> {
+                    item { Text(text = "Loading") }
+                }
+                loadState.append is LoadState.Error -> {
+                    item { Text(text = "Error") }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun CardPhoto(photo: PhotoItem?) {
+    Card(
+        elevation = CardDefaults.cardElevation(24.dp),
+        modifier = Modifier
+            .padding(start = 16.dp, end = 16.dp, top = 16.dp)
+    ) {
+        Column {
+            Row(Modifier.padding(top = 8.dp, bottom = 8.dp)) {
+                AsyncImage(
+                    model = photo!!.user!!.profileImage!!.large,
+                    contentDescription = "profileImage",
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier
+                        .width(48.dp)
+                        .padding(start = 16.dp)
+                )
+                Text(
+                    text = photo.user?.name ?: "null",
+                    modifier = Modifier.padding(start = 16.dp),
+                    fontSize = 18.sp
+                )
+            }
             AsyncImage(
                 model = photo!!.urls!!.regular,
                 contentDescription = "image",
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
             )
         }
+
     }
 }
